05738bb7db9b3c2447bd53faf566309a03621b15
ARGoncharov <gonlex@mail.ru>
2023-05-07 12:14:50 +0300

    Modifying the app

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Counter'
+            placeholderString: "Enter note name",
+            title: 'Counter',
+            inputValue: '',
+            notes: ['Заметка 1', 'Заметка 2']
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value;
+        },
+        addNewNote() {
+            this.notes.push(this.inputValue)
         }
     }
 }
 
-const app = Vue.createApp(App)
-
-app.mount('#app')
+Vue.createApp(App).mount('#app')
