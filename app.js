c77ac00bca141f8e11bfcc94ae26f54c5f764d4a
ARGoncharov <gonlex@mail.ru>
2023-05-07 21:35:15 +0300

    Adding business logic for the app, do optimization

--- app.js
+++ app.js
@@ -2,17 +2,44 @@ const App = {
     data() {
         return {
             placeholderString: "Enter note name",
-            title: 'Counter',
+            title: 'NoteCounter',
             inputValue: '',
-            notes: ['Заметка 1', 'Заметка 2']
+            notes: []
         }
     },
     methods: {
         inputChangeHandler(event) {
-            this.inputValue = event.target.value;
+            this.inputValue = event.target.value
         },
         addNewNote() {
-            this.notes.push(this.inputValue)
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        inputKeyPressed(event) {
+            if (event.key === 'Enter') {
+                this.addNewNote()
+            }
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            return this.notes.length * 2
+        },
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 50) {
+                this.inputValue = ''
+            }
+            console.log('input  value changed', value)
         }
     }
 }
